f9cd5e48eadc0e1f9be9ee12f1f2ab7e8f9b0b6b
CristianOlaya16 <Cristian.olaya.1220@gmail.com>
2025-05-24 23:52:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Formulario cargado!");
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/api/ultimo_ticket")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("id_ticket").value = data.nuevo_id;
+        })
+        .catch(error => console.error("Error al obtener el último ticket:", error));
+});
