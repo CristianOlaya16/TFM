7804291fbea21897275d47faf43ad44c33900ce8
CristianOlaya16 <Cristian.olaya.1220@gmail.com>
2025-05-25 00:04:12 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,14 @@
 const sql = require("mssql");
+const config = require("./db-config");
 
 module.exports = async function (context, req) {
-    const config = {
-        user: "estudiante",
-        password: "Examen@2024",
-        server: "srvbigdata.database.windows.net",
-        database: "TFM - TelcoMadrid",
-        options: { encrypt: true }
-    };
-
-   try {
+    try {
         await sql.connect(config);
-
-        // Obtener el último id_ticket
         const result = await sql.query`SELECT MAX(id_ticket) AS ultimo_id FROM reporte_soporte`;
         const nuevo_id = result.recordset[0].ultimo_id + 1;
-
         context.res = { body: { nuevo_id } };
     } catch (err) {
         context.res = { status: 500, body: err.message };
     }
 };
+
